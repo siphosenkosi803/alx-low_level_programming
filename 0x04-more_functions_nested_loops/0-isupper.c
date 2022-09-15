74c3c73eb707b69c937a7734fb723d6a7398cf52
Siphosenkosi Mlotshwa <sibusisoradebe943@yahoo.com>
2022-09-15 08:54:05 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*_isupper - checks for uppercase character
+*@c: the character to be checked
+*@c: the character to be checked
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
